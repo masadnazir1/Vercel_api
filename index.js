13a7af56248e2020532535c6de42febc54bd07b3
noetic-asad <asad@noeticworld.com>
2024-06-25 18:07:32 +0500

    added the products in the api

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const app = express()
 const PORT = 4000
 
 
-app.get('/home', (req, res) => {
-  res.status(200).json('Welcome, your app is working well');
+
+const products = [
+  { id: 1, name: 'Product 1', price: 10.99 },
+  { id: 2, name: 'Product 2', price: 19.99 },
+  { id: 3, name: 'Product 3', price: 5.99 },
+];
+
+
+
+app.get('/products', (req, res) => {
+  res.json(products);
 })
 
 
