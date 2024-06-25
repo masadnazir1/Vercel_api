fdae7b83bb18644fc22be8840b1e444f18aa35ea
noetic-asad <asad@noeticworld.com>
2024-06-25 18:14:48 +0500

    added the peron route

--- index.js
+++ index.js
@@ -11,11 +11,59 @@ const products = [
   { id: 3, name: 'Product 3', price: 5.99 },
 ];
 
+const person = [
 
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'},
+{First_Name : 'Muhammad asad Nazir', Last_name: 'Azad', age: '20'}
+
+];
 
 app.get('/products', (req, res) => {
   res.json(products);
 })
+
+
+
+app.get('/person', (req, res) => {
+  res.json(person);
+})
+
 
 
 app.listen(PORT, () => {
